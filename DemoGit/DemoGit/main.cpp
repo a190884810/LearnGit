88a11fbc265411b314f93bee04d4a07d5e1690e1
a190884810 <a190884810@gmail.com>
2019-11-07 12:51:41 -0600

    First Commit

---
+++ DemoGit/DemoGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DemoGit
+//
+//  Created by Hanson Xie on 7/11/19.
+//  Copyright © 2019年 Hanson Xie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ LearnGit/LearnGit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnGit
+//
+//  Created by Hanson Xie on 7/11/19.
+//  Copyright © 2019年 Hanson Xie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "First project for learning how to use git\n";
+    return 0;
+}
